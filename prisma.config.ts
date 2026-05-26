d3acf730af81dfd8a627d65ad2d9080601c61d58
ayushisehgal <sehgalayushi488@gmail.com>
2026-05-26 09:38:34 +0530

    fix: remove earlyAccess from prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,6 @@ import path from 'path'
 import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.join('prisma', 'schema.prisma'),
   migrate: {
     async adapter() {
